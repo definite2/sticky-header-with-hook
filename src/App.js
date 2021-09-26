1320d16422eeb65cfaa28e188983778ba1b6a4b3
definite2 <edefne878@gmail.com>
2021-09-26 19:45:37 +0300

    use scrollingUp hook create sticky header at scroll up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,134 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.scss";
+import useScrollingUp from "hooks/useScrollingUp";
 function App() {
+  const scrolledUp = useScrollingUp();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className={`${scrolledUp ? "stickyHeader" : ""}`}>
+        <h1>Header Title</h1>
       </header>
+      <nav > </nav>
+      <main>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+          vestibulum accumsan metus et efficitur. In nec mi est. Proin eget
+          pretium tortor. Fusce vulputate risus sit amet eros mollis, non
+          tincidunt diam tincidunt. Praesent mattis metus quis libero hendrerit
+          euismod. Sed convallis ligula nec feugiat rutrum. Proin quis metus
+          turpis. Sed in posuere velit.
+        </p>
+        <p>
+          Cras pulvinar luctus nulla in congue. Praesent id urna erat. Nulla
+          nisl neque, scelerisque at fermentum eu, porttitor ultrices urna. Cras
+          ligula neque, dignissim sit amet orci sit amet, ultrices faucibus
+          enim. Nulla ultrices gravida libero, quis cursus orci ultricies vitae.
+          Sed posuere orci id sapien efficitur, eget varius diam pellentesque.
+          Nam aliquet eros sit amet libero egestas fringilla. Curabitur auctor
+          vel justo vitae euismod.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+          vestibulum accumsan metus et efficitur. In nec mi est. Proin eget
+          pretium tortor. Fusce vulputate risus sit amet eros mollis, non
+          tincidunt diam tincidunt. Praesent mattis metus quis libero hendrerit
+          euismod. Sed convallis ligula nec feugiat rutrum. Proin quis metus
+          turpis. Sed in posuere velit.
+        </p>
+        <p>
+          Cras pulvinar luctus nulla in congue. Praesent id urna erat. Nulla
+          nisl neque, scelerisque at fermentum eu, porttitor ultrices urna. Cras
+          ligula neque, dignissim sit amet orci sit amet, ultrices faucibus
+          enim. Nulla ultrices gravida libero, quis cursus orci ultricies vitae.
+          Sed posuere orci id sapien efficitur, eget varius diam pellentesque.
+          Nam aliquet eros sit amet libero egestas fringilla. Curabitur auctor
+          vel justo vitae euismod.
+        </p>
+        <p>
+          Cras pulvinar luctus nulla in congue. Praesent id urna erat. Nulla
+          nisl neque, scelerisque at fermentum eu, porttitor ultrices urna. Cras
+          ligula neque, dignissim sit amet orci sit amet, ultrices faucibus
+          enim. Nulla ultrices gravida libero, quis cursus orci ultricies vitae.
+          Sed posuere orci id sapien efficitur, eget varius diam pellentesque.
+          Nam aliquet eros sit amet libero egestas fringilla. Curabitur auctor
+          vel justo vitae euismod.
+        </p>
+        <p>
+          Cras pulvinar luctus nulla in congue. Praesent id urna erat. Nulla
+          nisl neque, scelerisque at fermentum eu, porttitor ultrices urna. Cras
+          ligula neque, dignissim sit amet orci sit amet, ultrices faucibus
+          enim. Nulla ultrices gravida libero, quis cursus orci ultricies vitae.
+          Sed posuere orci id sapien efficitur, eget varius diam pellentesque.
+          Nam aliquet eros sit amet libero egestas fringilla. Curabitur auctor
+          vel justo vitae euismod.
+        </p>
+        <p>
+          Cras pulvinar luctus nulla in congue. Praesent id urna erat. Nulla
+          nisl neque, scelerisque at fermentum eu, porttitor ultrices urna. Cras
+          ligula neque, dignissim sit amet orci sit amet, ultrices faucibus
+          enim. Nulla ultrices gravida libero, quis cursus orci ultricies vitae.
+          Sed posuere orci id sapien efficitur, eget varius diam pellentesque.
+          Nam aliquet eros sit amet libero egestas fringilla. Curabitur auctor
+          vel justo vitae euismod.
+        </p>
+        <p>
+          Cras pulvinar luctus nulla in congue. Praesent id urna erat. Nulla
+          nisl neque, scelerisque at fermentum eu, porttitor ultrices urna. Cras
+          ligula neque, dignissim sit amet orci sit amet, ultrices faucibus
+          enim. Nulla ultrices gravida libero, quis cursus orci ultricies vitae.
+          Sed posuere orci id sapien efficitur, eget varius diam pellentesque.
+          Nam aliquet eros sit amet libero egestas fringilla. Curabitur auctor
+          vel justo vitae euismod.
+        </p>
+        <p>
+          Cras pulvinar luctus nulla in congue. Praesent id urna erat. Nulla
+          nisl neque, scelerisque at fermentum eu, porttitor ultrices urna. Cras
+          ligula neque, dignissim sit amet orci sit amet, ultrices faucibus
+          enim. Nulla ultrices gravida libero, quis cursus orci ultricies vitae.
+          Sed posuere orci id sapien efficitur, eget varius diam pellentesque.
+          Nam aliquet eros sit amet libero egestas fringilla. Curabitur auctor
+          vel justo vitae euismod.
+        </p>
+        <p>
+          Cras pulvinar luctus nulla in congue. Praesent id urna erat. Nulla
+          nisl neque, scelerisque at fermentum eu, porttitor ultrices urna. Cras
+          ligula neque, dignissim sit amet orci sit amet, ultrices faucibus
+          enim. Nulla ultrices gravida libero, quis cursus orci ultricies vitae.
+          Sed posuere orci id sapien efficitur, eget varius diam pellentesque.
+          Nam aliquet eros sit amet libero egestas fringilla. Curabitur auctor
+          vel justo vitae euismod.
+        </p>
+        <p>
+          Cras pulvinar luctus nulla in congue. Praesent id urna erat. Nulla
+          nisl neque, scelerisque at fermentum eu, porttitor ultrices urna. Cras
+          ligula neque, dignissim sit amet orci sit amet, ultrices faucibus
+          enim. Nulla ultrices gravida libero, quis cursus orci ultricies vitae.
+          Sed posuere orci id sapien efficitur, eget varius diam pellentesque.
+          Nam aliquet eros sit amet libero egestas fringilla. Curabitur auctor
+          vel justo vitae euismod.
+        </p>
+        <p>
+          Cras pulvinar luctus nulla in congue. Praesent id urna erat. Nulla
+          nisl neque, scelerisque at fermentum eu, porttitor ultrices urna. Cras
+          ligula neque, dignissim sit amet orci sit amet, ultrices faucibus
+          enim. Nulla ultrices gravida libero, quis cursus orci ultricies vitae.
+          Sed posuere orci id sapien efficitur, eget varius diam pellentesque.
+          Nam aliquet eros sit amet libero egestas fringilla. Curabitur auctor
+          vel justo vitae euismod.
+        </p>
+        <p>
+          Cras pulvinar luctus nulla in congue. Praesent id urna erat. Nulla
+          nisl neque, scelerisque at fermentum eu, porttitor ultrices urna. Cras
+          ligula neque, dignissim sit amet orci sit amet, ultrices faucibus
+          enim. Nulla ultrices gravida libero, quis cursus orci ultricies vitae.
+          Sed posuere orci id sapien efficitur, eget varius diam pellentesque.
+          Nam aliquet eros sit amet libero egestas fringilla. Curabitur auctor
+          vel justo vitae euismod.
+        </p>
+      </main>
+      <aside />
+      <footer>
+        <h1>Footer</h1>
+      </footer>
     </div>
   );
 }
